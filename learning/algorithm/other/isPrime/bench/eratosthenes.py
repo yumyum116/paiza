b92ce97a91b819df50d746d0c19bdd3288a9139e
yumyum116 <yu24miura@gmail.com>
2025-12-12 13:01:44 +0000

    done

---
+++ learning/algorithm/other/isPrime/bench/eratosthenes.py
@@ -0,0 +1,23 @@
+MAX_A = 6000000
+
+def eratosthenes(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    return is_prime
+
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+is_prime_table = eratosthenes(MAX_A)
+
+out = []
+for x in arr:
+    out.append("pass" if is_prime_table[x] else "failure")
+
+print("\n".join(out))
